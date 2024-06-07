4832b4953319dae869f2d47f45b94912d26a500d
Vinay Kumar <114856104+vinayparjapati5@users.noreply.github.com>
2024-06-07 16:45:06 +0530

    Update Set Matrix Zeroes.cpp

--- Day 1/Set Matrix Zeroes.cpp
+++ Day 1/Set Matrix Zeroes.cpp
@@ -1,1 +1,93 @@
+# Brute force
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void markRow(vector<vector<int>> &matrix, int n, int m, int i){
+  // set all non-zero elements as -1 in the row i:
+    for (int j = 0; j < m; j++) {
+        if (matrix[i][j] != 0) {
+            matrix[i][j] = -1;
+        }
+    }
+}
+
+void markCol(vector<vector<int>> &matrix, int n, int m, int j) {
+    // set all non-zero elements as -1 in the col j:
+    for (int i = 0; i < n; i++) {
+        if (matrix[i][j] != 0) {
+            matrix[i][j] = -1;
+        }
+    }
+}
+
+vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
+
+    // Set -1 for rows and cols
+    // that contains 0. Don't mark any 0 as -1:
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 0) {
+                markRow(matrix, n, m, i);
+                markCol(matrix, n, m, j);
+            }
+        }
+    }
+   // Finally, mark all -1 as 0:
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == -1) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    return matrix;
+}
+
+TIME COMPLEXITY : O((n*m)*(n+m)) + O(n*m)
+SPACE COMPLEXITY: O(1)
+=========================================================================================================
+
+  #BETTER APPROACH
+
+  void setZeroes(vector<vector<int>>& matrix) {
+
+        int n = matrix[0].size(); //total no. of columns
+        int m = matrix.size(); //total rows
+        
+
+        vector<int>row(m,0);
+        vector<int>col(n,0);
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(row[i]==1 || col[j]==1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+    }
+TIME COMPLEXITY : O(2*n*m)
+SPACE COMPLEXITY: O(n+m)
+=========================================================================================================
+
+#OPTIMAL APPROACH
+
+
+  
+
+
+
 

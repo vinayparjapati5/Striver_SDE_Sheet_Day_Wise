d5930b72eddab440134012687d6746ee7e9cebd5
Vinay Kumar <114856104+vinayparjapati5@users.noreply.github.com>
2024-06-08 08:40:39 +0530

    Update Set Matrix Zeroes.cpp

--- Day 1/Set Matrix Zeroes.cpp
+++ Day 1/Set Matrix Zeroes.cpp
@@ -54,15 +54,15 @@ SPACE COMPLEXITY: O(1)
 
   void setZeroes(vector<vector<int>>& matrix) {
 
-        int n = matrix[0].size(); //total no. of columns
-        int m = matrix.size(); //total rows
+        int m = matrix[0].size(); //total no. of columns
+        int n = matrix.size(); //total rows
         
 
-        vector<int>row(m,0);
-        vector<int>col(n,0);
+        vector<int>row(n,0);
+        vector<int>col(m,0);
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(matrix[i][j]==0){
                     row[i]=1;
                     col[j]=1;
@@ -70,8 +70,8 @@ SPACE COMPLEXITY: O(1)
             }
         }
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(row[i]==1 || col[j]==1){
                     matrix[i][j]=0;
                 }
@@ -83,7 +83,55 @@ TIME COMPLEXITY : O(2*n*m)
 SPACE COMPLEXITY: O(n+m)
 =========================================================================================================
 
-#OPTIMAL APPROACH
+#OPTIMAL APPROACH -- optimised space
+
+ //int row[n] = {0} = matrix[...][0]
+ //int col[m] = {0} = matrix[0][...]
+
+        int col0 = 1;
+
+        // Traverse
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }
+                    else col0 = 0;
+                }
+            }
+        }
+
+        // Mark with 0 from (1,1) to (n-1, m-1):
+
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        //Finally mark Ist row then Ist col
+
+        if(matrix[0][0]==0){
+            for(int j=0; j<m; j++){
+                matrix[0][j]=0;
+            }
+        }
+
+        if(col0 == 0){
+            for(int i=0; i<n; i++){
+                matrix[i][0]=0;
+            }
+        }
+
+TIME COMPLEXITY : O(2*n*m)
+SPACE COMPLEXITY: O(1)
+
 
 
   

ac542b56d87d46d6b02246f82b26b54c1f337b5b
Vinay Kumar <114856104+vinayparjapati5@users.noreply.github.com>
2024-07-23 23:19:42 +0530

    Create Pow(x,n).cpp

---
+++ Day 3/Pow(x,n).cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+       double ans = 1.0;
+       long long nn = n;
+
+      if(nn<0){
+        nn = -1 * nn;
+      }
+      for (int i = 0; i < nn; i++) {
+        ans = ans * x;
+      }
+
+      if(n<0){
+        ans =  (double) 1.0 / (double)ans;
+      }
+      return ans;
+
+
+        // Optimise
+      /*double ans = 1.0;
+      long long nn = n;
+
+      if(nn<0){
+        nn = -1 * nn;
+      }
+
+      while(nn!=0){
+        
+        if(nn%2 ==0){
+            x = x *x;
+            nn = nn/2;
+        }
+
+        else{
+            ans = ans* x;
+            nn = nn-1;
+        }
+      }
+
+      if(n<0){
+        ans = (double)1.0/(double)ans;
+      }
+
+      return ans; */
+    }
+};
